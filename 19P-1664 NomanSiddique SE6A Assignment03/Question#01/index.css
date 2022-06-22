421c19bbb5bb8d01756a6fb1af9b0a1815970f88
Noman_Siddique <74356264+Nomansiddiq@users.noreply.github.com>
2022-06-22 18:59:34 +0500

    Add files via upload

---
+++ 19P-1664 NomanSiddique SE6A Assignment03/Question#01/index.css
@@ -0,0 +1,14 @@
+function isCheck(){
+    var num = document.getElementById('evenodd').value;
+   
+    if((num%2) == 0){
+    document.getElementById('showMssg').innerHTML = "Number "+num+"  is an even number..!";
+    }
+    else{
+    document.getElementById('showMssg').innerHTML = "Number "+num+"  is an odd number..!";
+    }
+   
+    if(num == ""){
+    document.getElementById('showMssg').innerHTML = "";
+    }
+   }
---
+++ 19P-1664 NomanSiddique SE6A Assignment03/Question#02/index.js
@@ -0,0 +1,15 @@
+
+function isCheck(){
+    var num = document.getElementById('val').value;
+for(var x=0; x <=num; x++)
+{
+   for (var y=0; y < x; y++)
+     {
+    var chr=chr+("*");     
+      
+      }
+    console.log(chr);
+    chr='';
+}
+ 
+}
